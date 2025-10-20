8c5a7fdb724a76ba78f6e6140d492b52e4a3e4cd
Georges Jentgen <georges@polysign.lu>
2025-10-20 21:56:41 +0200

    chore: bump version to 0.0.2

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -769,7 +769,7 @@ export class AppHome {
         <section class="card about-card">
           <header>
             <span class="about-title">{translations.aboutTitle}</span>
-            <span class="about-version">v0.0.1</span>
+            <span class="about-version">v0.0.2</span>
           </header>
           <p class="about-text">{translations.aboutDescription}</p>
           <footer class="about-footer">
